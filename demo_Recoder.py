bdecfc57aedc2b4301cd04ab228b549e2e2f7c83
Souvik Mondal <48649959+Frantic-13@users.noreply.github.com>
2023-12-16 23:20:56 +0530

    Update demo_Recoder.py

--- demo_Recoder.py
+++ demo_Recoder.py
@@ -2,10 +2,10 @@ import cv2 as cv
 import numpy as np
 
 # Distance constants 
-KNOWN_DISTANCE = 45 #INCHES
-PERSON_WIDTH = 16 #INCHES
-MOBILE_WIDTH = 3.0 #INCHES
-
+KNOWN_DISTANCE = 70 #CM
+PERSON_WIDTH = 55 #CM
+MOBILE_WIDTH = 9 #CM
+FOCAL_LENGTH = 500 #cm
 # Object detector constant 
 CONFIDENCE_THRESHOLD = 0.4
 NMS_THRESHOLD = 0.3
@@ -39,19 +39,20 @@ def object_detector(image):
         # define color of each, object based on its class id 
         color= COLORS[int(classid) % len(COLORS)]
     
-        label = "%s : %f" % (class_names[classid[0]], score)
+        label = "%s : %f" % (class_names[classid], score)
 
         # draw rectangle on and label on object
         cv.rectangle(image, box, color, 2)
-        cv.putText(image, label, (box[0], box[1]-14), FONTS, 0.5, color, 2)
+        # cv.putText(image, label, (box[0], box[1]-14), FONTS, 0.5, color, 2)
     
         # getting the data 
         # 1: class name  2: object width in pixels, 3: position where have to draw text(distance)
         if classid ==0: # person class id 
-            data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
+            data_list.append([class_names[classid], box[2], (box[0], box[1]-2)])
         elif classid ==67:
-            data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        # return list 
+            data_list.append([class_names[classid], box[2], (box[0], box[1]-2)])
+        # if you want inclulde more classes then you have to simply add more [elif] statements here
+        # returning list containing the object data. 
     return data_list
 
 def focal_length_finder (measured_distance, real_width, width_in_rf):
@@ -64,46 +65,34 @@ def distance_finder(focal_length, real_object_width, width_in_frmae):
     distance = (real_object_width * focal_length) / width_in_frmae
     return distance
 
-# reading the reference image from dir 
-ref_person = cv.imread('ReferenceImages/image14.png')
-ref_mobile = cv.imread('ReferenceImages/image4.png')
 
-mobile_data = object_detector(ref_mobile)
-mobile_width_in_rf = mobile_data[1][1]
-
-person_data = object_detector(ref_person)
-person_width_in_rf = person_data[0][1]
-
-print(f"Person width in pixels : {person_width_in_rf} mobile width in pixel: {mobile_width_in_rf}")
-
-# finding focal length 
-focal_person = focal_length_finder(KNOWN_DISTANCE, PERSON_WIDTH, person_width_in_rf)
-
-focal_mobile = focal_length_finder(KNOWN_DISTANCE, MOBILE_WIDTH, mobile_width_in_rf)
-cap = cv.VideoCapture(3)
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-Recoder = cv.VideoWriter('out.mp4', fourcc,8.0,(int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))) )
+cap = cv.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
-    data = object_detector(frame) 
+    x_cord = 75  # x-coordinate of the top-left corner of the cropped region
+    y_cord = 20  # y-coordinate of the top-left corner of the cropped region
+    width = 475  # Width of the cropped region
+    height = 440
+
+    cropped_frame = frame[y_cord:y_cord+height, x_cord:x_cord+width]
+
+    data = object_detector(cropped_frame) 
     for d in data:
         if d[0] =='person':
-            distance = distance_finder(focal_person, PERSON_WIDTH, d[1])
+            distance = distance_finder(FOCAL_LENGTH, PERSON_WIDTH, d[1])
             x, y = d[2]
         elif d[0] =='cell phone':
-            distance = distance_finder (focal_mobile, MOBILE_WIDTH, d[1])
+            distance = distance_finder (FOCAL_LENGTH, MOBILE_WIDTH, d[1])
             x, y = d[2]
         cv.rectangle(frame, (x, y-3), (x+150, y+23),BLACK,-1 )
-        cv.putText(frame, f'Dis: {round(distance,2)} inch', (x+5,y+13), FONTS, 0.48, GREEN, 2)
+        cv.putText(frame, f'Dis: {round(distance,2)} CM', (x+5,y+13), FONTS, 0.48, GREEN, 2)
 
+    cv.rectangle(frame, (x_cord, y_cord), (x_cord+width, y_cord+height),(255, 0, 0),2)
     cv.imshow('frame',frame)
-    Recoder.write(frame)
-
     
     key = cv.waitKey(1)
     if key ==ord('q'):
         break
 cv.destroyAllWindows()
-Recoder.release()
 cap.release()
